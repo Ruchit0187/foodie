1bdb6e1e578783c6811e8a6601fb796fa0477c96
ruchit <example@gmail.com>
2026-01-27 10:27:23 +0530

    check for token by console log

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,15 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-
 export async function middleware(request: NextRequest) {
-  const secret = process.env.JWT_SECRET_KEY;
+  const secret = process.env.NEXTAUTH_SECRET!;
   const email = request.cookies.get("email")?.value;
   const path = request.nextUrl.pathname;
   const token = await getToken({ req: request, secret });
   if (!email && path.startsWith("/resetpassword")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+  console.log({ token });
   if (!token && path.startsWith("/admin")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
