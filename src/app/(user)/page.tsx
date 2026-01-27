e1854fe199e0890c67d35cab82eb67f7077041fd
ruchit <example@gmail.com>
2026-01-27 10:31:32 +0530

    dummy console for prdo

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 function HomePage() {
+  console.log("homepagre loadoed");
   return (
     <div className="relative w-full h-[87vh]">
       <Image
