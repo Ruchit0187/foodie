d2efa0a4f258589f5c9526c9bc88eba86d1975b8
ruchit <example@gmail.com>
2026-02-12 16:12:23 +0530

    remove the await in the visitiors route

--- src/app/api/visitiors/route.ts
+++ src/app/api/visitiors/route.ts
@@ -1,15 +1,15 @@
 import { headers } from "next/headers";
 
 export async function GET() {
-  const headersList =await headers();
+  const headersList =  headers();
 
-  const ip =  headersList.get("x-forwarded-for")?.split(",")[0] || "Unknown";
+  const ip = headersList.get("x-forwarded-for")?.split(",")[0] || "Unknown";
 
   const country = headersList.get("x-vercel-ip-country") || "Unknown";
   const region = headersList.get("x-vercel-ip-country-region") || "Unknown";
   const city = headersList.get("x-vercel-ip-city") || "Unknown";
   const continent = headersList.get("x-vercel-ip-continent") || "Unknown";
-
+  console.log(headersList);
   return Response.json({
     ip,
     continent,
