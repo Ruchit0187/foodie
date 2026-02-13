5bbe6affd82eaf58969ed42efbbcdafbdfd83926
ruchit <example@gmail.com>
2026-02-13 17:30:50 +0530

    updateRecipe add the withCredientials

--- src/components/UpdateRecipe.tsx
+++ src/components/UpdateRecipe.tsx
@@ -48,10 +48,14 @@ function UpdateRecipe({ value }: { value: recipeDataTypes }) {
   };
   const onSubmit = async (data: recipeDataTypes) => {
     try {
-      const value = await axios.patch("/api/admin/recipes", {
-        ...data,
-        recipeID,
-      });
+      const value = await axios.patch(
+        "/api/admin/recipes",
+        {
+          ...data,
+          recipeID,
+        },
+        { withCredentials: true },
+      );
       if (value.status === 200) {
         reset(data);
         router.refresh();
